778fcced3c3c2f5bb54434c030ca53d227290064
Nikhil Chourasia <139704756+nikhil-chourasia@users.noreply.github.com>
2024-12-15 13:30:55 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-
+from rembg import remove
+from PIL import Image
+import os
+folderPath = input("Enter the file location here with file name as 'input.jpg': ")
+input_path = 'input.jpg'
+output_path = os.path.join(folderPath, 'output.png')
+input_image = Image.open(os.path.join(folderPath, input_path))
+output_image = remove(os.path.join(folderPath, input_path))
+output_image.save(output_path)
